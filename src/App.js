70aeffe1da13af93ac0160594ee617de0bb404f0
AndySS <andy_ss_22@icloud.com>
2021-09-27 16:14:14 -0300

    lab done but images dont reload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './assets/style/style.css';
+
+import Header from "../src/components/Header";
+import HeroText from "../src/components/HeroText";
+import Feature from "../src/components/Feature";
+import ReactLogo from "../src/components/ReactLogo";
+
+const featuresArray = [
+  {
+    title: "Declarative",
+    image: "/public/images/icon1.png",
+    description: "React makes it painless to create interactive UIs.",
+  },
+  {
+    title: "Components",
+    image: "/public/images/icon2.png",
+    description: "Build encalpsulated components that manage their state",
+  },
+  {
+    title: "Single-way",
+    image: "/public/images/icon3.png",
+    description: "A set of immutable values are passed to the components",
+  },
+  {
+    title: "JSX",
+    image: "/public/images/icon4.png",
+    description: "Statically typed, designed to run on modern browsers",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <Header />
+      <div className="flex-container">
+        <HeroText />
+        <ReactLogo top="10%" right="30%" scale="1.3" />
+        <ReactLogo top="42%" right="16%" scale="0.9" />
+        <ReactLogo top="65%" right="30%" scale="0.9" />
+        <ReactLogo top="10%" right="-7%" scale="0.7" />
+        <ReactLogo top="38%" right="-7%" scale="0.7" />
+        <ReactLogo top="65%" right="-7%" scale="0.7" />
+      </div>
+      <div className="features-container">
+        {featuresArray.map((feature) => (
+          <Feature
+            key={feature.title}
+            title={feature.title}
+            image={feature.image}
+            description={feature.description}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+       <header className="header">
+        <img alt='Ironhack Logo' scr='/public/images/ironhack-logo.svg' />
+        <img alt='Menu icon' src='/public/images/menu-top.svg' />
+       </header>
+    );
+}
+
+export default Header;
